0f4fd93792631118816f259324e0165f1dbaf094
Cyril Tiger <cyriltiger@gmail.com>
2021-09-30 18:25:01 +0200

    added navigation component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,25 +1,13 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
 import Logo from '../logo'
-
+import Navigation from '../navigation'
 
 export default class Header extends React.Component {
   render() {
     return (
       <header>
         <Logo />
-        <nav>
-          <ul>
-            <li>
-              <NavLink exact to="/">
-                Accueil
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/about">A propos</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Navigation />
       </header>
     )
   }
