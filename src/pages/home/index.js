971fe3915e8040f6472b42f3af3c11888cdf5a77
Cyril Tiger <cyriltiger@gmail.com>
2021-10-01 20:07:16 +0200

    WIP added fetch json

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,11 +10,48 @@ export default class Home extends React.Component {
         partout et ailleurs
       </h2>
     )
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    }
   }
+
+  componentDidMount() {
+    fetch('./data/data.json')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result
+          })
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
+  // Create component for list + location card
+  createLocationList() {
+    return (
+      <ul>
+        {this.state.data.map((location) => (
+          <li key={location.id}>{location.title}</li>
+        ))}
+      </ul>
+    )
+  }
+
   render() {
     return (
       <main className="home">
         <Banner title={this.title} />
+        {this.createLocationList()}
       </main>
     )
   }
