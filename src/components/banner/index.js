62eba65c9460a6c8f78cd422917b89df8caf948a
Cyril Tiger <cyriltiger@gmail.com>
2021-10-01 18:37:33 +0200

    passed banner text as props from home to banner

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -3,13 +3,6 @@ import '../../styles/banner.css'
 
 export default class Banner extends React.Component {
   render() {
-    return (
-      <div className="banner">
-        <h2>
-          Chez vous, <br />
-          partout et ailleurs
-        </h2>
-      </div>
-    )
+    return <div className="banner">{this.props.title}</div>
   }
 }
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,9 +3,15 @@ import Banner from '../../components/banner'
 
 export default class Home extends React.Component {
   render() {
+    const title = (
+      <h2>
+        Chez vous, <br />
+        partout et ailleurs
+      </h2>
+    )
     return (
       <main>
-        <Banner />
+        <Banner title={title} />
       </main>
     )
   }
