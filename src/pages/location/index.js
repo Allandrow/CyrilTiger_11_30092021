3e70b5d01ef4de83487a68e7e16dcbe8b378b811
Cyril Tiger <cyriltiger@gmail.com>
2021-10-02 16:07:33 +0200

    added fetch data of location after routing

--- src/pages/location/index.js
+++ src/pages/location/index.js
@@ -1,8 +1,40 @@
 import React from 'react'
 
 export default class Location extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: null,
+      isLoaded: false,
+      data: []
+    }
+    this.id = this.props.match.params.id
+  }
+
+  componentDidMount() {
+    fetch('../data/data.json')
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            data: result.find((location) => location.id === this.id)
+          })
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          })
+        }
+      )
+  }
+
   render() {
-    console.log(this.props.match.params.id)
-    return <h2>Hello</h2>
+    return (
+      <main>
+        <h2>{this.state.data.title}</h2>
+      </main>
+    )
   }
 }
