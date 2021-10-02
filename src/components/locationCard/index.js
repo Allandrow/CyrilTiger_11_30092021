3d6d36e13b4663809452c0f326e91ee248aace3c
Cyril Tiger <cyriltiger@gmail.com>
2021-10-02 17:14:43 +0200

    added data for location cards + styles

--- src/components/locationCard/index.js
+++ src/components/locationCard/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../../styles/locationCard.css'
 
 export default class LocationCard extends React.Component {
   render() {
+    console.log(this.props.location)
     return (
       <li>
-        <Link to={`/location/${this.props.location.id}`}>
+        <Link
+          to={`/location/${this.props.location.id}`}
+          className="locationCard"
+        >
+          <div className="imgGradient">
+            <img
+              src={this.props.location.cover}
+              alt={this.props.location.title}
+            />
+          </div>
           <h3>{this.props.location.title}</h3>
         </Link>
       </li>
