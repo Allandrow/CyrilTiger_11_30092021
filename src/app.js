30fbdf060b39312f57db70c44403ddb2836ec1f1
Cyril Tiger <cyriltiger@gmail.com>
2021-10-04 12:47:50 +0200

    added app.js to wrap content without footer

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Error from './pages/error'
+import Header from './components/header'
+import Location from './pages/location'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/location/:id" component={Location} />
+          <Route path="*">
+            <Error />
+          </Route>
+        </Switch>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,30 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/reset.css'
 import './index.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Home from './pages/home'
-import About from './pages/about'
-import Error from './pages/error'
-import Header from './components/header'
-import Location from './pages/location'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Footer from './components/footer'
+import App from './app'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/location/:id" component={Location} />
-        <Route path="*">
-          <Error />
-        </Route>
-      </Switch>
+      <App />
       <Footer />
     </Router>
   </React.StrictMode>,
